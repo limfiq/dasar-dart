f064929507be5c6191ec6cc9a7b0fbd186a0be1d
limfiq <mtaufiq39@gmail.com>
2022-05-12 23:06:43 +0700

    variabel
    melengkapi isi penggunaan variabel

---
+++ 1.variabel.dart
@@ -0,0 +1,17 @@
+String nama = "Ufiq";
+int umur = 20;
+double berat = 60.5;
+bool isMarried = false;
+String nama2 = "Linda";
+int umur2 = 30;
+bool isMarried2 = true;
+void main(List<String> args) {
+  if (isMarried == true) {
+    print("Nama saya $nama");
+    print("Umur saya $umur");
+  } else {
+    print("Nama saya $nama2");
+    print("Umur saya $umur2");
+    print("Status saya $isMarried2");
+  }
+}
