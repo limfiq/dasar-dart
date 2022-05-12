a87894cb48944dd8602b7f3036554168ac8f4a8f
limfiq <mtaufiq39@gmail.com>
2022-05-12 23:36:11 +0700

    tipe data
    melengkapi type data

--- typedata.dart
+++ typedata.dart
@@ -1,12 +1,17 @@
-String nama = "Ufiq";
-int umur = 20;
+String nama = "Hull";
+int umur = 27;
 double berat = 60.5;
-bool isMarried = false;
-String nama2 = "Linda";
+bool isMarried = true;
+String nama2 = "Ufiq";
 int umur2 = 30;
 bool isMarried2 = true;
+List<String> namaList = ["Ufiq", "Hull"];
+Map<String, int> umurMap = {
+  "Ufiq": 30,
+  "Hull": 27,
+};
 
- main() {
+main() {
   if (isMarried == true) {
     print("Nama saya $nama");
     print("Umur saya $umur");
@@ -14,5 +19,7 @@ bool isMarried2 = true;
     print("Nama saya $nama2");
     print("Umur saya $umur2");
     print("Status saya $isMarried2");
+   
   }
+
 }
