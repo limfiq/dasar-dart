ac42fc8751362d1981ad5f5b004b7f9e1e522bda
limfiq <mtaufiq39@gmail.com>
2022-09-28 13:18:42 +0700

    ini adalah koding utama dalam dart yang menjadi dasar dari suatu project dart dan flutter

---
+++ 1. awal.dart
@@ -0,0 +1,7 @@
+/// void main(){} adalah coding utama yang harus ada pada setiap project karena
+/// file yang ada koding tersebut yang akan dieksekusi
+/// untuk menampilkan string bisa dengan petik dua ("") atau petik satu('')
+/// tiap akhir koding diakhiri dengan titik koma (;)
+void main() {
+  print('Coba Dart');
+}
