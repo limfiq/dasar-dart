d466f90315ab2673d1b537be08ee51de1091ba29
limfiq <mtaufiq39@gmail.com>
2022-10-24 10:00:32 +0700

    Update 4.1.ternaryoperator.dart

--- Pertemuan 4/4.1.ternaryoperator.dart
+++ Pertemuan 4/4.1.ternaryoperator.dart
@@ -2,12 +2,13 @@ import 'dart:io';
 
 void main() {
   var nilai = 60;
-  // String keterangan;
   String keterangan = nilai >= 75 ? 'Anda Lulus' : 'Anda Gagal';
+  print(keterangan);
+
+  // String keterangan;
   // if (nilai >= 75) {
   //   keterangan = 'Anda Lulus';
   // } else {
   //   keterangan = 'Anda Gagal';
   // }
-  print(keterangan);
 }
